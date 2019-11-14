f4dde6cf1e8db530d669c4294341bbd61fa59a4c
Malik Bagwala <m.bagwala@gmail.com>
2019-11-14 21:54:51 +0530

    added thumbnail

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\mbagw\SDKs\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\mbagw\Development\android\bmi_calculator"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\mbagw\SDKs\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
